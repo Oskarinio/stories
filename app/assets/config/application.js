723ae1336ffd10b0dfcdf535fc209f6cfc0729d7
Oskarinio <zielasoon@gmail.com>
2019-11-26 15:49:15 +0100

    homepage with side-menu on js

---
+++ app/assets/config/application.js
@@ -0,0 +1,8 @@
+$(".icon-menu").click(
+  function() {
+  $(".menu").animate({ left: '0px' }, "slow")
+  });
+$(".icon-close").click(
+  function() {
+  $(".menu").animate({ left: '-240px' }, "slow")
+  });
