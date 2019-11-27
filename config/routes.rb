a19daeca71dd99f08e6e64258a88ae4c7f8df4a6
Oskarinio <zielasoon@gmail.com>
2019-11-27 22:43:31 +0100

    form added works

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :signups
   root 'pages#home'
 
   get '/pages/about' => 'pages#about', as: :about
+  get '/signups/new' => 'signups#new'
+  get 'pages/thanks' => 'pages#thanks', as: :thanks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
